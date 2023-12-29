045de467c58c45fed03be30eb134155f6610451c
green-dino <104174275+green-dino@users.noreply.github.com>
2023-12-28 21:51:46 -0700

    metrics

--- Sentement/pdf.py
+++ Sentement/pdf.py
@@ -25,53 +25,70 @@ def generate_word_cloud(text):
     st.pyplot(fig)
 
 # Define the function for interactive sentiment analysis
-def interactive_sentiment_analysis(analyzer):
+def interactive_sentiment_analysis(analyzer, selected_text):
     if st.checkbox("Interactive Sentiment Analysis"):
-        # Allow users to select a sentence or paragraph for analysis
-        selected_text = st.text_area("Enter text for sentiment analysis:", value="")
         sentiment_score = analyzer.sentiment_analyzer.polarity_scores(selected_text)['compound']
         st.write("Sentiment Score:", sentiment_score)
 
 # Define the function for keyword extraction
 def extract_keywords(text, num_keywords=5):
-    # Tokenize the text
     words = word_tokenize(text)
-
-    # Remove stop words
     stop_words = set(stopwords.words('english'))
     filtered_words = [word.lower() for word in words if word.isalnum() and word.lower() not in stop_words]
-
-    # Calculate word frequencies
     word_freq = FreqDist(filtered_words)
-
-    # Get the most common keywords
     keywords = word_freq.most_common(num_keywords)
-
     return keywords
+
+# Define the function for metrics calculation and visualization
+def calculate_and_visualize_metrics(text):
+    words = word_tokenize(text)
+    word_count = len(words)
+
+    sentences = sent_tokenize(text)
+    sentence_lengths = [len(word_tokenize(sentence)) for sentence in sentences]
+
+    paragraphs = [p.strip() for p in text.split('\n\n') if p.strip()]
+    paragraph_lengths = [len(word_tokenize(paragraph)) for paragraph in paragraphs]
+
+    # Plotting
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 10))
+
+    ax1.bar(["Word Count"], [word_count], color='blue')
+    ax1.set_ylabel("Count")
+
+    ax2.hist(sentence_lengths, bins=20, color='green', edgecolor='black')
+    ax2.set_xlabel("Sentence Length")
+    ax2.set_ylabel("Frequency")
+
+    ax3.hist(paragraph_lengths, bins=20, color='orange', edgecolor='black')
+    ax3.set_xlabel("Paragraph Length")
+    ax3.set_ylabel("Frequency")
+
+    plt.tight_layout()
+    st.pyplot(fig)
 
 # Streamlit app
 def main():
     st.title("PDF Word Cloud Generator")
 
-    # File uploader
     uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
     if uploaded_file is not None:
-        # Extract text from the PDF
         pdf_text = extract_text(uploaded_file)
-
-        # Analyze the text
         analyzer = TextAnalyzer(pdf_text)
 
-        # Display sentiment analysis results (optional)
         st.subheader("Sentiment Analysis Results:")
         st.write("Sentiment Score:", analyzer.sentiment_analyzer.polarity_scores(pdf_text)['compound'])
 
-        # Additional options
-        interactive_sentiment_analysis(analyzer)
-        extract_keywords(pdf_text)  # Corrected this line
+        interactive_sentiment_analysis(analyzer, st.text_area("Enter text for sentiment analysis:", value=""))
 
-        # Generate and display the word cloud
+        st.subheader("Metrics:")
+        calculate_and_visualize_metrics(pdf_text)
+
+        st.subheader("Keywords:")
+        keywords = extract_keywords(pdf_text)
+        st.write("Top Keywords:", keywords)
+
         st.subheader("Word Cloud:")
         generate_word_cloud(pdf_text)
 
